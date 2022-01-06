3d4fed45672870070a99dcf1813cde8774d00033
Pavels Demjanenko <pavel.demjanenko@gmail.com>
2022-01-06 15:18:07 +0200

    Setting up the app

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>This page uses React, Redux, React Router and many other helpful libraries.</p>
+  </div>
+);
+
+export default AboutPage;
---
+++ webpack.config.dev.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+process.env.NODE_ENV = 'development';
+
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  devtool: 'cheap-module-source-map',
+  entry: './src/index',
+  output: {
+    //__dirname will give current directory name
+    path: path.resolve(__dirname, "build"),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    stats: 'minimal',
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: {"Access-Control-Allow-Origin": "*"},
+    https: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico"
+    })
+  ],
+  //tell webpack what files we want to handle
+  module: {
+    rules: [
+      //will run babel on all javascript files
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /mode_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      //will run babel on css
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
