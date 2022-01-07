a197b65c14822d76fed4da033dd0f8bfa8651456
Pavels Demjanenko <pavel.demjanenko@gmail.com>
2022-01-07 19:54:16 +0200

    Setting up state and dispatch

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
-import Header from "./common/Header";
-import PageNotFound from "./PageNotFound";
-import CoursesPage from "./courses/CoursesPage";
+import Header from './common/Header';
+import PageNotFound from './PageNotFound';
+import CoursesPage from './courses/CoursesPage';
 
 function App() {
   return (
