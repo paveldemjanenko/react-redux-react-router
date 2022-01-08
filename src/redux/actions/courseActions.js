b63aa479f1061061b4a6adb603269d3884fdd02b
Pavels Demjanenko <pavel.demjanenko@gmail.com>
2022-01-08 12:23:18 +0200

    Adding save thunk and manage course page

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,12 +1,16 @@
 import * as types from './actionTypes';
 import * as courseApi from '../../api/courseApi';
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course: course };
-}
-
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses: courses };
+}
+
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course: course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course: course };
 }
 
 //thunks
@@ -16,6 +20,23 @@ export function loadCourses() {
       .getCourses()
       .then(courses => {
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  //seconf prop allows to access redux store directly
+  // return function(dispatch, getState) {
+  return function(dispatch) {
+    return courseApi
+      .saveCourse(course)
+      .then(saveCourse => {
+        course.id
+        ? dispatch(updateCourseSuccess(saveCourse))
+        : dispatch(createCourseSuccess(saveCourse));
       })
       .catch(error => {
         throw error;
